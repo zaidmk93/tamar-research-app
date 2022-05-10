0e01b45c5edb27efa263ee439aeb64eb4e03ffbe
Lior <liorieiz1@gmail.com>
2022-05-10 10:00:48 +0300

    preparing for first deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { DataService } from './shared/services/data.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [DataService],
+  providers: [DataService, RouterModule],
 })
 export class AppComponent {
   title = 'research-tasks-multi-lang';
+
 }
